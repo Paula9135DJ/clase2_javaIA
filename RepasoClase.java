a4b928b6591032f20253e1e1ac01a351438c3aa5
Paula9135DJ <andre9135@gmail.com>
2026-06-13 16:01:58 -0500

    actualizado1

--- RepasoClase.java
+++ RepasoClase.java
@@ -2,13 +2,28 @@ public class RepasoClase{
 
     public static void main(String[] args){
 
-        String Nombre = "Paula";
+        /*String Nombre = "Paula";
         int edad = 38;
         double estatura = 1.65;
 
         System.out.println("Nombre " + Nombre );
         System.out.println("Edad: " + edad);
         System.out.println("Estatura: "+ estatura);
+        */
+       //final double IVA = 0.19;
+      // final int DIAS_SEMANA = 7;
+
+      int entero = 5;
+      double decimal = entero;
+
+      int entero2 = 5;
+
+      System.out.println( "suma: " +  (entero + entero2));
+
+      System.out.println("valor entero: " + entero);
+      System.out.println("valor decimal: "+ decimal);
+
+
 
     }
 }
