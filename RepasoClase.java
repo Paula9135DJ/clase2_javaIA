3a8dc3f11a75f6edc826edcc25c801bc7cb3610f
Paula9135DJ <andre9135@gmail.com>
2026-06-13 15:16:54 -0500

    archivo de repaso

---
+++ RepasoClase.java
@@ -0,0 +1,14 @@
+public class RepasoClase{
+
+    public static void main(String[] args){
+
+        String Nombre = "Paula";
+        int edad = 38;
+        double estatura = 1.65;
+
+        System.out.println("Nombre " + Nombre );
+        System.out.println("Edad: " + edad);
+        System.out.println("Estatura: "+ estatura);
+
+    }
+}
